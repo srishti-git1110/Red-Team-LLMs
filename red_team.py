f7c3b2de0a9aaec31b090c3c9df0d9efeabb1d5f
Srishti Gureja <84127491+srishti-git1110@users.noreply.github.com>
2023-10-03 01:10:33 +0530

    Update red_team.py

--- red_team.py
+++ red_team.py
@@ -1,1 +1,32 @@
+from pydantic import BaseModel
+from transformers import pipeline, TextGenerationPipeline
+import argparse
+
+class CandidatePrompt(BaseModel):
+  '''
+  Represents a single (adversarial) prompt.
+
+  Attributes:
+    text: str - the textual prompt
+    scores: list[float] - list of scores for each token in the prompt [-R(xi)]
+    own_score: Optional[float] - 
+  '''
+  text: str
+  scores: list[float]
+  own_score: Optional[float] = None
+
+  def mean(self):
+    return sum(self.scores) / len(self.scores)
+
+  def std(self):
+    return (sum((score - self.mean()) ** 2 for score in self.scores) / len(self.scores)) ** 0.5
+
+  def __str__(self):
+    return f'{self.text[:40]} ({self.mean():.3f} ± {self.std():.3f})'
+
+  # figure out hashing based on self.text
+
+
+class PromptPool(BaseModel):
+  
 
